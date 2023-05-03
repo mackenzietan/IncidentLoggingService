9f46a0ab0e4be9c691f3b9592cb9206fb000e606
Mac Tantlinger <mactantlinger@gmail.com>
2023-05-03 15:35:20 -0400

    add preliminary db connection and psuedo for commitlog event

--- IncidentLoggingService/MainWindow.xaml.cs
+++ IncidentLoggingService/MainWindow.xaml.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient; 
+
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -25,9 +30,32 @@ namespace IncidentLoggingService
             InitializeComponent();
         }
 
+        static void DbConnect()
+        {
+            string connectionString;
+            connectionString = @"Data Source=ISAL05;Initial Catalog=ILS;User ID=sa;Password=Dev3560?";
+
+            SqlConnection cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+        }
+
+        static void DbDisconnect()
+        {
+            string connectionString;
+            connectionString = @"Data Source=ISAL05;Initial Catalog=ILS;User ID=sa;Password=Dev3560?";
+
+            SqlConnection cnn = new SqlConnection(connectionString);
+
+            cnn.Close();
+        }
+
         private void commitLog_Click(object sender, RoutedEventArgs e)
         {
-
+            //here we want to call another method that adds the entered values in the db
+            //process: open, add, close
         }
+
+
     }
 }
