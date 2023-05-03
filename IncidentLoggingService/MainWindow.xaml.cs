8b6a13d3c88fef3d19c4009f91cec849a293640d
Mac Tantlinger <mactantlinger@gmail.com>
2023-05-03 14:11:52 -0400

    add commit log button and event handler

--- IncidentLoggingService/MainWindow.xaml.cs
+++ IncidentLoggingService/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace IncidentLoggingService
         {
             InitializeComponent();
         }
+
+        private void commitLog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
